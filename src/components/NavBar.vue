e74498cdf115c794bd8c4ebe3dbc61e3a7a2cd33
Alex Younger <thealexyounger@proton.me>
2023-10-01 14:55:35 -0400

    feat: remove default components and add navigation components

---
+++ src/components/NavBar.vue
@@ -0,0 +1,12 @@
+<template>
+    <header class="body-font">
+        <div class="container border-b border-slate-800 mx-auto flex flex-wrap py-4 flex-col md:flex-row items-center">
+            <a href="/" class="flex title-font font-medium items-center text-gold mb-4 md:mb-0">
+                <span class="ml-3 text-xl">The Tenth Man</span>
+            </a>
+            <nav class="md:ml-auto flex flex-wrap items-center justify-center">
+            <a class="mr-5 text-gold hover:text-yellow-500">Blog</a>
+            </nav>
+        </div>
+    </header>
+</template>
---
+++ src/views/BlogView.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container max-w-5xl">
+        {{ markdown }}
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'BlogView',
+    data() {
+        return {
+            markdown: ''
+        }
+    },
+    mounted() {
+        this.fetchMarkdown();
+    },
+    methods: {
+        async fetchMarkdown() {
+            const markdown = await fetch('/blog/2021-05-01-why-i-started-this-blog.md');
+            this.markdown = await markdown.text();
+        }
+    }
+    
+}
+</script>
