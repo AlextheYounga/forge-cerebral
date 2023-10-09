0a75b75e7589769bd24543ffa5a911225084bf7f
Alex Younger <thealexyounger@proton.me>
2023-10-09 01:47:45 -0400

    build: updating packages and adding comments for vite markdown

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// import markdownVitePlugin, {Mode} from 'vite-plugin-markdown'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    // markdownVitePlugin({mode: [Mode.HTML]}),
   ],
   resolve: {
     alias: {
