f7659963537c706645eb734714a2cd4dabaf995a
Alex Younger <thealexyounger@proton.me>
2023-10-09 01:49:12 -0400

    feat: add metadata script

--- scripts/generateArticleDirectory.js
+++ scripts/generateArticleDirectory.js
@@ -1,13 +1,52 @@
 // node scripts/generateArticleDirectory.js
 const fs = require('fs')
 const _ = require('lodash')
-const articles = require('../src/data/articles.json')
 const jsdom = require("jsdom");
+
+const METAMAPPING = {
+    "og:site_name": "site_name",
+    "original-source": 'og_source',
+    "og:title": 'title',
+    "og:type": 'type',
+    "og:image": 'image',
+    "og:description": 'description',
+}
+
+function parseMetaFromArticle(content) {
+    const html = new jsdom.JSDOM(content).window;
+
+    const head = html.document.getElementsByTagName('head')[0]
+    const title = head.getElementsByTagName('title')[0].textContent
+
+    const metaData = {}
+    const useHeadData = {
+        title: title,
+        meta: []
+    }
+
+    const metaTags = head.getElementsByTagName('meta');
+
+    for (const tag of metaTags) {
+        const name = tag.getAttribute('name')
+        const content = tag.getAttribute('content')
+
+        const mappedName = METAMAPPING[name] ?? name
+        metaData[mappedName] = content
+
+        useHeadData.meta.push({
+            name: name,
+            content: content
+        })
+    }
+
+    return { useHeadData, metaData }
+}
 
 const blogFolder = 'src/blog'
 const blogTopics = fs.readdirSync(blogFolder)
 
 const blogDirectory = []
+const blogHead = {}
 const blogList = []
 
 // Loop through categories
@@ -32,37 +71,29 @@ for (const topic of blogTopics) {
 
         const postSlug = post.replace('.md', '')
         const postContent = fs.readFileSync(postFullpath, 'utf-8')
-        const postPath = `${topic}/${postSlug}`
+        const postPath = `/blog/${topic}/${postSlug}`
 
         // Parse head meta content
-        const doc = new jsdom.JSDOM(postContent).window;
-        const head = doc.document.getElementsByTagName('head')[0]
-        const postTitle = head.getElementsByTagName('title')[0].textContent
-        const postSubtitle = head.querySelector('meta[name="subtitle"]')?.getAttribute('content')
-        const description = head.querySelector('meta[name="description"]')?.getAttribute('content')
-        const postDate = head.querySelector('meta[name="date"]')?.getAttribute('content')
-        const postImg = head.querySelector('meta[name="og:image"]')?.getAttribute('content')
+        const { useHeadData, metaData } = parseMetaFromArticle(postContent)
+        useHeadData.href = postPath
+        metaData.href = postPath
 
-        let blogItem = {
-            name: _.startCase(postTitle),
-            category: topicDisplayTitle,
-            href: `/blog/${postPath}`,
-            subtitle: postSubtitle ?? '',
-            description: description ?? '',
-            date: postDate ?? '',
-            img: postImg ?? '',
-        }
+        // Add to blog list
+        blogList.push(metaData)
 
-        // Add to list
-        blogList.push(blogItem)
+        // Add to useHead list
+        if (!blogHead[postPath]) blogHead[postPath] = []
+        blogHead[postPath] = useHeadData
 
         // Add to directory
-        topicDirectory.children.push(blogItem)
+        topicDirectory.children.push(metaData)
     }
 
     blogDirectory.push(topicDirectory)
 }
+
 fs.writeFileSync('src/data/articles.json', JSON.stringify(blogList, null, 4))
+fs.writeFileSync('src/data/metadata.json', JSON.stringify(blogHead, null, 4))
 fs.writeFileSync('src/data/directory.json', JSON.stringify(blogDirectory, null, 4))
 console.log('Article directory generated!')
 
