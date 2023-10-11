e19c05764d5597f77d00cb135e594188a04fddf4
Alex Younger <thealexyounger@proton.me>
2023-10-11 11:18:03 -0400

    build: remove polyfills plugins from vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import generateArticleMetadata from './plugins/articleMeta'
-// import markdownVitePlugin, {Mode} from 'vite-plugin-markdown'
-// markdownVitePlugin({mode: [Mode.HTML]}),
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -22,18 +19,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  optimizeDeps: {
-    esbuildOptions: {
-      // Node.js global to browser globalThis
-      define: {
-        global: 'globalThis'
-      },
-      // Enable esbuild polyfill plugins
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true
-        })
-      ]
-    }
-  }
 })
